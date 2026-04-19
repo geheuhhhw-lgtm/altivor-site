819e27cfc9052688285fd680e9a3b4e257fc6b78
geheuhhhw-lgtm <geheuhhhw@gmail.com>
2026-04-19 12:26:45 +0200

    Create page.js

---
+++ app/login/page.js
@@ -0,0 +1,40 @@
+'use client'
+
+import { useState } from 'react'
+import { supabase } from '@/lib/supabase'
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    console.log('DATA:', data)
+    console.log('ERROR:', error)
+  }
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Login</h1>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  )
+}
